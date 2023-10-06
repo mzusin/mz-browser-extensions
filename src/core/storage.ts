348a5516aa5d0aaf3689e3302c77535f585be065
Miriam Zusin <miriamzusin@gmail.com>
2023-10-06 14:04:49 +0300

    session data

--- src/core/storage.ts
+++ src/core/storage.ts
@@ -51,6 +51,35 @@ export const getLocalData = <T>(key: string) : Promise<T|null> => {
     });
 };
 
+/**
+ * Used for sensitive data.
+ * Holds data in memory for the duration of a browser session.
+ * By default, it's not exposed to content scripts,
+ * but this behavior can be changed by setting chrome.storage.session.setAccessLevel().
+ * The storage limit is 10 MB (1 MB in Chrome 111 and earlier).
+ */
+export const getSessionData = <T>(key: string) : Promise<T|null> => {
+
+    return new Promise((resolve, reject) => {
+        try{
+            chrome.storage.session.get([key], result => {
+                if(!result || !result[key]) {
+                    resolve(null);
+                    return;
+                }
+
+                const _result = JSON.parse(result[key]) as T;
+
+                resolve(_result);
+            });
+        }
+        catch (ex) {
+            console.error(ex);
+            reject(ex);
+        }
+    });
+};
+
 export const getAllSyncData = () => {
 
     return new Promise((resolve, reject) => {
@@ -125,6 +154,25 @@ export const setLocalData = <T>(key: string, data: T) : Promise<void> => {
     });
 };
 
+export const setSessionData = <T>(key: string, data: T) : Promise<void> => {
+
+    return new Promise((resolve, reject) => {
+        try{
+            const _data = JSON.stringify(data);
+
+            chrome.storage.session.set({
+                [key]: _data,
+            }, () => {
+                resolve();
+            });
+        }
+        catch (ex) {
+            console.error(ex);
+            reject(ex);
+        }
+    });
+};
+
 export const deleteSyncData = (key: string) => {
     return new Promise((resolve, reject) => {
         try{
@@ -151,6 +199,19 @@ export const deleteLocalData = (key: string) => {
     });
 };
 
+export const deleteSessionData = (key: string) => {
+    return new Promise((resolve, reject) => {
+        try{
+            chrome.storage.session.remove(key, () => {
+                resolve(null);
+            });
+        }
+        catch (ex) {
+            reject(ex);
+        }
+    });
+};
+
 export const clearSyncStorage = () => {
     return new Promise((resolve, reject) => {
         try{
@@ -168,6 +229,19 @@ export const clearLocalStorage = () => {
     return new Promise((resolve, reject) => {
         try{
             chrome.storage.local.clear(() => {
+                resolve(null);
+            });
+        }
+        catch (ex) {
+            reject(ex);
+        }
+    });
+};
+
+export const clearSessionStorage = () => {
+    return new Promise((resolve, reject) => {
+        try{
+            chrome.storage.session.clear(() => {
                 resolve(null);
             });
         }
